f732ff3c094d6e67ae23f0366a30320e3408c754
Victor Ospina <vospina@alltic.co>
2025-01-15 00:27:24 -0500

    Initial Project

---
+++ scripts/save_data.js
@@ -0,0 +1,17 @@
+let botonEnviar = document.getElementById('solicitudcun:btnEnviar');
+botonEnviar.addEventListener('click', function guardarDatos(event) {
+    console.log("Hizo Click")
+    let iden = document.getElementById('solicitudcun:iden').value;
+    let mail = document.getElementById('solicitudcun:mail').value;
+    let ticu = document.getElementById('solicitudcun:ticu').value;
+    let tiqu = document.getElementById('solicitudcun:tiqu').value;
+    let obje = document.getElementById('solicitudcun:obje').value;
+    let fund = document.getElementById('solicitudcun:fund').value;
+
+    // Guardar datos en localStorage
+    localStorage.setItem('solicitud', JSON.stringify({
+        iden, mail, ticu, tiqu, obje, fund
+    }));
+    console.log('Datos guardados!');
+    console.log(localStorage.getItem('solicitud'))
+});
---
+++ scripts/send_email.js
@@ -0,0 +1,36 @@
+
+const solicitud = JSON.parse(localStorage.getItem('solicitud'));
+console.log("Recuperacion Datos")
+console.log(solicitud)
+var cunNo = document.getElementById("respuestacun:cunNo").innerText;
+console.log("test cun No: " + cunNo);
+
+const correo = {
+    from: "noreply@emtel.com.co",
+    to: solicitud.mail,
+    subject: `Confirmación de CUN: ${cunNo}`,
+    content: `Estimado usuario,\n\nSu CUN generado es: ${cunNo}\n\nCopia peticion:\n\n${solicitud.obje}\n\n${solicitud.fund}\n\nGracias por su solicitud.`
+};
+
+// Llamada a la API de ZeptoMail
+fetch('https://api.zeptomail.com/v1.1/email', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Zoho-enczapikey wSsVR61xrBT5Cfx9nGKlI+86nFRWAV3+Ek4r2FXz6nf1G//HpcdplkTKUwamSvcbF2dtQjdE8rIvzhgHhjsN29QlzQ0FCCiF9mqRe1U4J3x17qnvhDzCX29flxaMJI8Mwg1rmWBiGskm+g=='
+    },
+    body: JSON.stringify({
+        from: { address: correo.from },
+        to: [{ email_address: { address: correo.to } }, { email_address: { address: "servicioalcliente@emtel.com.co" } }],
+        subject: correo.subject,
+        textbody: correo.content
+    })
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log('Correo enviado:', data);
+    })
+    .catch((error) => {
+        console.error('Error al enviar correo:', error);
+    });
